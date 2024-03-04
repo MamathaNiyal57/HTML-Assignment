1bc8c8dbe54b54a5c4675c94efc5e4294f447e29
MamathaNiyal57 <160117937+MamathaNiyal57@users.noreply.github.com>
2024-03-04 11:23:18 +0530

    Adding Js file also updated html file

---
+++ index.js
@@ -0,0 +1,28 @@
+const m=document.querySelector(".change");
+function red()
+{
+    m.style.color="red";
+    // m.innerHTML="RED";
+}
+
+function blue()
+{
+    m.style.color="blue";
+    
+}
+
+function green()
+{
+    m.style.color="green";
+    // m.innerHTML="RED";
+}
+
+function pink()
+{
+    m.style.color="pink";
+    // m.innerHTML="RED";
+}
+
+
+
+
